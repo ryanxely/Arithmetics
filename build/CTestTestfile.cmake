490b9954de115bdb12116a43badb2ad274c8f3ed
Ryan <ryanxely@gmail.com>
2026-04-29 03:00:39 +0100

    Building and Testing

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/MyLyfe/Domains/SecDom/Activities/Perso/FullStackEngineering/LanguageBased/C&C++/XelyCalc/Arithmetics
+# Build directory: E:/MyLyfe/Domains/SecDom/Activities/Perso/FullStackEngineering/LanguageBased/C&C++/XelyCalc/Arithmetics/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
